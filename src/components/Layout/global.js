c47b658e9f9f9aa1f02aa49d961126d2f77ec61f
Daniel Soares <daniel.soares@me.com>
2020-07-06 13:03:38 -0700

    adding title styles

--- src/components/Layout/global.js
+++ src/components/Layout/global.js
@@ -12,6 +12,14 @@ const GlobalStyles = createGlobalStyle`
     font-size: 20px;
     line-height: 1.5;
   }
+
+  h1, h2, h3, h4 {
+    font-family: 'Krona one', sans-serif;
+  }
+
+  h2 {
+    font-size: 40px;
+  }
 `
 
 export default GlobalStyles
