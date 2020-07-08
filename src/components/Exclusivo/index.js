f051a73478eab5dea74efb8f0394c98402f36d47
Daniel Soares <daniel.soares@me.com>
2020-07-07 19:12:55 -0700

    adding Exclusivo component

---
+++ src/components/Exclusivo/index.js
@@ -0,0 +1,43 @@
+import React from "react"
+
+import * as S from "./styled"
+
+const Exclusivo = () => (
+  <S.ExperienceWrapper>
+    <S.Image />
+    <S.TextWrapper>
+      <h2>
+        <span className="small">Exclusivo,</span>
+        <br />
+        seguro, anti-inflação
+      </h2>
+
+      <p>Investimento IMUNE à quebra de bancos;</p>
+
+      <p>NÃO PODE ser congelado pelo governo;</p>
+
+      <p>
+        PROTEÇÃO direta para o PODER DE COMPRA do seu dinheiro, para
+        investimentos de incorporação imobiliária ou aluguéis de imóveis.
+      </p>
+
+      <h2>
+        <span className="small">Especialização</span>
+        <br />
+        transparência
+      </h2>
+
+      <p>
+        Empresa com ampla especialidade em empreendimentos imobiliários a preço
+        de custo (Lei 4.591/64);
+      </p>
+
+      <p>
+        Acesso direto a todas as informações referentes ao empreendimento para
+        os associados, donos do negócio.
+      </p>
+    </S.TextWrapper>
+  </S.ExperienceWrapper>
+)
+
+export default Exclusivo
---
+++ src/components/Exclusivo/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+export const ExperienceWrapper = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  background: black;
+  color: white;
+`
+
+export const TextWrapper = styled.div`
+  margin: 90px 10%;
+`
+
+export const Image = styled.div`
+  background: grey;
+`
