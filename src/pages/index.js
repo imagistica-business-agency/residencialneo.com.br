928ba89cb4470b3858a65b8732794953a3d41c9e
Daniel Soares <daniel.soares@me.com>
2020-07-07 19:15:17 -0700

    adding new components to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,26 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import Nav from "../components/Nav"
+import Header from "../components/Header"
+import SubHeader from "../components/SubHeader"
+import Plantas from "../components/Plantas"
+import Experience from "../components/Experience"
+import Exclusivo from "../components/Exclusivo"
+import Diferenciais from "../components/Diferenciais"
 import Associados from "../components/Associados"
 import Novidades from "../components/Novidades"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Nav />
+    <Header />
+    <SubHeader />
+    <Plantas />
+    <Experience />
+    <Exclusivo />
+    <Diferenciais />
     <Associados />
     <Novidades />
   </Layout>
