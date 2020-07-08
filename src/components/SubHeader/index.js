d887d774b011da109f07169f54b7c01b889717c9
Daniel Soares <daniel.soares@me.com>
2020-07-07 19:14:00 -0700

    adding SubHeader component

---
+++ src/components/SubHeader/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import * as S from "./styled"
+
+const SubHeader = () => (
+  <S.SubHeaderWrapper>
+    <S.Image>No coração de Foz do Iguaçu</S.Image>
+    <S.Image>Localização privilegiada</S.Image>
+    <S.Image>Ótimo investimento</S.Image>
+    <S.Image>Tranquilidade e segurança</S.Image>
+  </S.SubHeaderWrapper>
+)
+
+export default SubHeader
---
+++ src/components/SubHeader/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+export const SubHeaderWrapper = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 30px;
+  padding: 100px 30px;
+`
+
+export const Image = styled.div`
+  padding: 100px 20%;
+  font-family: "Krona one";
+  text-align: center;
+  color: white;
+  background: gray;
+`
