a1650a7ce1fd8d9714f42b59504b6376b27dd69b
Daniel Soares <daniel.soares@me.com>
2020-07-07 19:13:17 -0700

    adding Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import * as S from "./styled"
+
+const IndexPage = () => <S.HeaderWrapper />
+
+export default IndexPage
---
+++ src/components/Header/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+export const HeaderWrapper = styled.header`
+  background: gray;
+  height: 100vh;
+`
