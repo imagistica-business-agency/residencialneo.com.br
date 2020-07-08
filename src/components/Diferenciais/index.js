986bcfec62d2dc8d91f42a0282318fd5dfb493c4
Daniel Soares <daniel.soares@me.com>
2020-07-07 19:12:31 -0700

    adding Diferenciais component

---
+++ src/components/Diferenciais/index.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+import * as S from "./styled"
+
+const Diferenciais = () => (
+  <S.DiferenciaisWrapper>
+    <S.TextWrapper>
+      <h2>
+        <span className="small">Diferenciais e vantagens</span>
+        <br />
+        VR Investimentos
+      </h2>
+
+      <p>
+        O Sistema VR Investimentos é próprio (concebido pela própria VR), único
+        (só existe esse no mercado brasileiro) e exclusivo (só a VR tem),
+        sistematizado e aperfeiçoado continuamente.
+      </p>
+
+      <p>
+        Possuímos know-how técnico, financeiro, jurídico e administrativo de
+        mais de uma década.
+      </p>
+    </S.TextWrapper>
+    <S.Image />
+  </S.DiferenciaisWrapper>
+)
+
+export default Diferenciais
---
+++ src/components/Diferenciais/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const DiferenciaisWrapper = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`
+
+export const TextWrapper = styled.div`
+  margin: 280px 10%;
+`
+
+export const Image = styled.div`
+  background: grey;
+`
