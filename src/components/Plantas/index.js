b679502e51b7208dd33810a8528a3bf1eb7e96e7
Daniel Soares <daniel.soares@me.com>
2020-07-27 13:39:39 -0300

    adding tabs

--- src/components/Plantas/index.js
+++ src/components/Plantas/index.js
@@ -5,10 +5,17 @@ import Modal from "../../components/Modal"
 import * as S from "./styled"
 
 const Plantas = () => {
-  const { planta } = useStaticQuery(
+  const data = useStaticQuery(
     graphql`
       query {
-        planta: file(relativePath: { eq: "63m.jpg" }) {
+        first: file(relativePath: { eq: "63m.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1920, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        second: file(relativePath: { eq: "74m.jpg" }) {
           childImageSharp {
             fluid(maxWidth: 1920, quality: 100) {
               ...GatsbyImageSharpFluid_withWebp
@@ -19,50 +26,99 @@ const Plantas = () => {
     `
   )
 
-  const image = planta.childImageSharp.fluid
+  const firstImage = data.first.childImageSharp.fluid
+
+  const secondImage = data.second.childImageSharp.fluid
 
   return (
     <S.PlantasWrapper id="plantas">
-      <S.TextWrapper>
-        <h2>Modelos e plantas</h2>
+      <h2>Modelos e plantas</h2>
 
-        <table style={{ width: "100%" }}>
-          <tbody>
-            <tr>
-              <td>ÁREA PRIVATIVA</td>
-              <td style={{ textAlign: "right" }}>63,38m2</td>
-            </tr>
-            <tr>
-              <td>ÁREA GARAGEM</td>
-              <td style={{ textAlign: "right" }}>66,52m2</td>
-            </tr>
-            <tr>
-              <td>ÁREA TOTAL</td>
-              <td style={{ textAlign: "right" }}>129,90m2</td>
-            </tr>
-            <tr>
-              <td>SUÍTE</td>
-              <td style={{ textAlign: "right" }}>SIM</td>
-            </tr>
-            <tr>
-              <td>QUARTOS</td>
-              <td style={{ textAlign: "right" }}>2</td>
-            </tr>
-            <tr>
-              <td>VARANDA</td>
-              <td style={{ textAlign: "right" }}>SIM</td>
-            </tr>
-            <tr>
-              <td>VAGA DE GARAGEM</td>
-              <td style={{ textAlign: "right" }}>SIM</td>
-            </tr>
-          </tbody>
-        </table>
-        <S.ButtonWrapper>
-          <Modal ButtonText="Agende sua visita" />
-        </S.ButtonWrapper>
-      </S.TextWrapper>
-      <S.Image src="../../images/63m.jpg" />
+      <S.PlantaTabs>
+        <S.PlantaTabList>
+          <S.PlantaTab>63m²</S.PlantaTab>
+          <S.PlantaTab>74m²</S.PlantaTab>
+        </S.PlantaTabList>
+
+        <S.PlantaTabPanel>
+          <S.TextWrapper>
+            <table style={{ width: "100%" }}>
+              <tbody>
+                <tr>
+                  <td>ÁREA PRIVATIVA</td>
+                  <td style={{ textAlign: "right" }}>63,38m2</td>
+                </tr>
+                <tr>
+                  <td>ÁREA GARAGEM</td>
+                  <td style={{ textAlign: "right" }}>66,52m2</td>
+                </tr>
+                <tr>
+                  <td>ÁREA TOTAL</td>
+                  <td style={{ textAlign: "right" }}>129,90m2</td>
+                </tr>
+                <tr>
+                  <td>SUÍTE</td>
+                  <td style={{ textAlign: "right" }}>SIM</td>
+                </tr>
+                <tr>
+                  <td>QUARTOS</td>
+                  <td style={{ textAlign: "right" }}>2</td>
+                </tr>
+                <tr>
+                  <td>VARANDA</td>
+                  <td style={{ textAlign: "right" }}>SIM</td>
+                </tr>
+                <tr>
+                  <td>VAGA DE GARAGEM</td>
+                  <td style={{ textAlign: "right" }}>SIM</td>
+                </tr>
+              </tbody>
+            </table>
+          </S.TextWrapper>
+          <S.Image fluid={firstImage} />
+        </S.PlantaTabPanel>
+
+        <S.PlantaTabPanel>
+          <S.TextWrapper>
+            <table style={{ width: "100%" }}>
+              <tbody>
+                <tr>
+                  <td>ÁREA PRIVATIVA</td>
+                  <td style={{ textAlign: "right" }}>74,1m2</td>
+                </tr>
+                <tr>
+                  <td>ÁREA GARAGEM</td>
+                  <td style={{ textAlign: "right" }}>67,47m2</td>
+                </tr>
+                <tr>
+                  <td>ÁREA TOTAL</td>
+                  <td style={{ textAlign: "right" }}>141,58m2</td>
+                </tr>
+                <tr>
+                  <td>SUÍTE</td>
+                  <td style={{ textAlign: "right" }}>SIM</td>
+                </tr>
+                <tr>
+                  <td>QUARTOS</td>
+                  <td style={{ textAlign: "right" }}>2</td>
+                </tr>
+                <tr>
+                  <td>VARANDA</td>
+                  <td style={{ textAlign: "right" }}>SIM</td>
+                </tr>
+                <tr>
+                  <td>VAGA DE GARAGEM</td>
+                  <td style={{ textAlign: "right" }}>SIM</td>
+                </tr>
+              </tbody>
+            </table>
+          </S.TextWrapper>
+          <S.Image fluid={secondImage} />
+        </S.PlantaTabPanel>
+      </S.PlantaTabs>
+      <S.ButtonWrapper>
+        <Modal ButtonText="Agende sua visita" />
+      </S.ButtonWrapper>
     </S.PlantasWrapper>
   )
 }
--- src/components/Plantas/styled.js
+++ src/components/Plantas/styled.js
@@ -1,8 +1,57 @@
 import styled from "styled-components"
 import media from "styled-media-query"
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs"
+import Img from "gatsby-image"
 
 export const PlantasWrapper = styled.section`
+  margin: 80px 10%;
+
+  ${media.lessThan("890px")`
+    grid-template-columns: 1fr;
+  `}
+`
+
+export const PlantaTabs = styled(Tabs)``
+
+export const PlantaTabList = styled(TabList)`
+  display: flex;
+  list-style-type: none;
+  margin-bottom: 40px;
+`
+
+export const PlantaTab = styled(Tab)`
+  background: transparent;
+  border: 1px solid black;
+  padding: 10px 60px;
+  margin-right: 20px;
+  font-family: inherit;
+  font-size: 14px;
+
+  cursor: pointer;
+  transition: 0.5s;
+
+  &:hover {
+    background: black;
+    color: white;
+  }
+
+  &.white {
+    color: white;
+    border-color: white;
+
+    &:hover {
+      background: white;
+      color: black;
+    }
+  }
+
+  &.react-tabs__tab--selected {
+    background: black;
+    color: white;
+  }
+`
+
+export const PlantaTabPanel = styled(TabPanel)`
   display: grid;
   grid-template-columns: 1fr 1fr;
 
@@ -11,20 +60,10 @@ export const PlantasWrapper = styled.section`
   `}
 `
 
-export const PlantaTab = styled(Tab)``
-
-export const PlantaTabs = styled(Tabs)``
-
-export const PlantaTabList = styled(TabList)``
-
-export const PlantaTabPanel = styled(TabPanel)``
-
-export const TextWrapper = styled.div`
-  margin: 80px 10%;
-`
+export const TextWrapper = styled.div``
 
 export const ButtonWrapper = styled.div`
   margin-top: 40px;
 `
 
-export const Image = styled.img``
+export const Image = styled(Img)``
