79f26c83a5b564837dc675391035883f8290c858
joel machado <joel.machado5@gmail.com>
2020-10-22 14:44:51 -0300

    imagistica
    url downloads

--- src/components/Downloads/index.js
+++ src/components/Downloads/index.js
@@ -8,12 +8,12 @@ const Downloads = () => (
 
     <ul>
       <li>
-        <a href="Logotipo.pdf" alt="Logotipo" target="_blank">
+        <a href="https://residencialneo.com.br/Logotipo.pdf" alt="Logotipo" target="_blank">
           Logotipo
         </a>
       </li>
       <li>
-        <a href="disponibilidade-outubro.pdf" alt="Tabela de adesão" target="_blank">
+        <a href="https://residencialneo.com.br/Disponibilidade-outubro.pdf" alt="Tabela de adesão" target="_blank">
           Tabela de adesão
         </a>
       </li>
@@ -28,12 +28,12 @@ const Downloads = () => (
         </a>
       </li>
       <li>
-        <a href="FolderDigital.pdf" alt="Folder digital" target="_blank">
+        <a href="https://residencialneo.com.br/FolderDigital.pdf" alt="Folder digital" target="_blank">
           Folder digital
         </a>
       </li>
       <li>
-        <a href="FolderImpresso.pdf" alt="Folder impresso" target="_blank">
+        <a href="https://residencialneo.com.br/FolderImpresso.pdf" alt="Folder impresso" target="_blank">
           Folder impresso
         </a>
       </li>
@@ -43,7 +43,7 @@ const Downloads = () => (
     <ul>
       <li>
         <a
-          href="StoriesInstagram1.png"
+          href="https://residencialneo.com.br/StoriesInstagram1.png"
           alt="Stories Instagram 1"
           target="_blank"
         >
@@ -52,7 +52,7 @@ const Downloads = () => (
       </li>
       <li>
         <a
-          href="StoriesInstagram2.png"
+          href="https://residencialneo.com.br/StoriesInstagram2.png"
           alt="Stories Instagram 2"
           target="_blank"
         >
@@ -61,7 +61,7 @@ const Downloads = () => (
       </li>
       <li>
         <a
-          href="StoriesInstagram3.png"
+          href="https://residencialneo.com.br/StoriesInstagram3.png"
           alt="Stories Instagram 3"
           target="_blank"
         >
@@ -69,52 +69,52 @@ const Downloads = () => (
         </a>
       </li>
       <li>
-        <a href="FeedInstagram1.png" alt="Feed Instagram 1" target="_blank">
+        <a href="https://residencialneo.com.br/FeedInstagram1.png" alt="Feed Instagram 1" target="_blank">
           Feed Instagram 1
         </a>
       </li>
       <li>
-        <a href="FeedInstagram2.png" alt="Feed Instagram 2" target="_blank">
+        <a href="https://residencialneo.com.br/FeedInstagram2.png" alt="Feed Instagram 2" target="_blank">
           Feed Instagram 2
         </a>
       </li>
       <li>
-        <a href="FeedInstagram3.png" alt="Feed Instagram 3" target="_blank">
+        <a href="https://residencialneo.com.br/FeedInstagram3.png" alt="Feed Instagram 3" target="_blank">
           Feed Instagram 3
         </a>
       </li>
       <li>
-        <a href="FeedFacebook1.png" alt="Feed Facebook 1" target="_blank">
+        <a href="https://residencialneo.com.br/FeedFacebook1.png" alt="Feed Facebook 1" target="_blank">
           Feed Facebook 1
         </a>
       </li>
       <li>
-        <a href="FeedFacebook2.png" alt="Feed Facebook 2" target="_blank">
+        <a href="https://residencialneo.com.br/FeedFacebook2.png" alt="Feed Facebook 2" target="_blank">
           Feed Facebook 2
         </a>
       </li>
       <li>
-        <a href="FeedFacebook3.png" alt="Feed Facebook 3" target="_blank">
+        <a href="https://residencialneo.com.br/FeedFacebook3.png" alt="Feed Facebook 3" target="_blank">
           Feed Facebook 3
         </a>
       </li>
       <li>
-        <a href="CapaFacebook1.png" alt="Capa Facebook 1" target="_blank">
+        <a href="https://residencialneo.com.br/CapaFacebook1.png" alt="Capa Facebook 1" target="_blank">
           Capa Facebook 1
         </a>
       </li>
       <li>
-        <a href="CapaFacebook2.png" alt="Capa Facebook 2" target="_blank">
+        <a href="https://residencialneo.com.br/CapaFacebook2.png" alt="Capa Facebook 2" target="_blank">
           Capa Facebook 2
         </a>
       </li>
       <li>
-        <a href="TV1.png" alt="TV 1" target="_blank">
+        <a href="https://residencialneo.com.br/TV1.png" alt="TV 1" target="_blank">
           TV 1
         </a>
       </li>
       <li>
-        <a href="TV2.png" alt="TV 2" target="_blank">
+        <a href="https://residencialneo.com.br/TV2.png" alt="TV 2" target="_blank">
           TV 2
         </a>
       </li>
