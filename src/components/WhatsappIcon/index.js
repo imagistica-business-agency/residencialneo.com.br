55293926092cf9b918ab9d11f4f520392177fc93
joel machado <joel.machado5@gmail.com>
2020-11-19 11:50:45 -0300

    icon

--- src/components/WhatsappIcon/index.js
+++ src/components/WhatsappIcon/index.js
@@ -5,13 +5,12 @@ import * as S from './styled'
 
 const WhatsappIcon = () => (
   <S.WhatsappWrapper>
-    <a
-      href="https://api.whatsapp.com/send?phone=5545991368889&text=Ol%C3%A1,%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20*Residencial%20NEO*"
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <img src={whatsappIcon} alt="" />
-    </a>
+
+
+<a href="https://api.whatsapp.com/send?phone=5545991368889&text=Ol%C3%A1,%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20*Residencial%20NEO*" class="float bounce" target="_blank">
+<img src={whatsappIcon} alt="" />
+</a>
+   
   </S.WhatsappWrapper>
 )
 
--- src/components/WhatsappIcon/styled.js
+++ src/components/WhatsappIcon/styled.js
@@ -6,7 +6,48 @@ export const WhatsappWrapper = styled.div`
   right: 20px;
   z-index: 1000;
 
-  img {
-    width: 40px;
-  }
+  footer {
+   
+    position: relative;
+    z-index: 10 !important;
+}
+.float{
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:40px;
+	right:40px;
+	background-color:#25d366;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+  font-size:30px;
+	box-shadow: 2px 2px 3px #999;
+  z-index:999 !important;
+}
+
+.my-float{
+	margin-top:16px;
+}
+
+.bounce {
+    animation: bounce 1s infinite;
+  
+}
+
+@keyframes bounce {
+    0%,
+    25%,
+    50%,
+    75%,
+    100% {
+        transform: translateX(0);
+    }
+    40% {
+        transform: translateX(-10px);
+    }
+    60% {
+        transform: translateX(-15px);
+    }
+}
 `
